3f1fb86f9f4254ce3dbef45a96d88f34c8b74912
DongRyeong Lee <dr11291129@gmail.com>
2022-11-11 15:04:41 +0900

    chore: rebase

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import Intro from "../pages/Intro";
-import QrReading from "../pages/QrReader";
+import QrReading from "../pages/QrReading";
 
 // 라우트명은 kebab-case 로 작성합니다
 
