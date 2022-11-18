bbad5b7d08b020917550ef5da5fa9df48ccde20b
seung <dmdgpdi@naver.com>
2022-11-18 19:40:15 +0900

    feat: 사진에 따른 설명 추가

--- src/pages/Nft/components/title.jsx
+++ src/pages/Nft/components/title.jsx
@@ -1,26 +1,38 @@
 const getTitle = (nftid) => {
-    let title;
-    switch(nftid){
-        case 1:{
-            title = "인하대"
-            break;
-        }
-        case 2:{
-            title = "비행기"
-            break;
-        }
-        default:{
-            title = "알수없음"
-            break;
-        }
+  let title;
+  switch (nftid) {
+    case 1: {
+      title = "정석도서관";
+      break;
     }
-    return title;
-}
+    case 2: {
+      title = "인하대";
+      break;
+    }
+    case 3: {
+      title = "겨울의 인경호";
+      break;
+    }
+    case 4: {
+      title = "가을의 인경호";
+      break;
+    }
+    case 4: {
+      title = "인하대";
+      break;
+    }
+    default: {
+      title = "정석오거리";
+      break;
+    }
+  }
+  return title;
+};
 
-const Title = ({nftid}) => {
-    let result = getTitle(parseInt(nftid));
+const Title = ({ nftid }) => {
+  let result = getTitle(parseInt(nftid));
 
-    return(result)
-}
+  return result;
+};
 
 export default Title;
